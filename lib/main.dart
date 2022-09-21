c693dfe607bf78d85238220efee3b47d04c4f6c2
aninarafath6 <arafathanin6@gmail.com>
2022-09-21 16:45:33 +0530

    splash screen stable✅

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Learn net',
       initialRoute: SplashView.routeName,
       onGenerateRoute: RouteUtil.onGenerateRoute,
+      debugShowCheckedModeBanner: false,
       // theme: ThemeData(
       //   primarySwatch: Colors.blue,
       // ),
---
+++ lib/views/onboarding/onboarding_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class OnboardingView extends StatelessWidget {
+  const OnboardingView({Key? key}) : super(key: key);
+  static const routeName = "/onboarding";
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
